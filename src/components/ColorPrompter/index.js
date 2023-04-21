d87751b6f63e6cf553dcba85c92ca52b5fafb7e0
Noa Kligfeld <noakligfeld@college.harvard.edu>
2023-04-21 11:06:18 -0400

    actually the code is in this commit

---
+++ src/components/ColorPrompter/index.js
@@ -0,0 +1,59 @@
+import { React, useState } from 'react'
+import { Configuration, OpenAIApi } from "openai";
+
+
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+const ColorPrompter = () => {
+    const [colorInput, setColorInput] = useState("");
+    const [adjectiveInput, setAdjectiveInput] = useState("");
+    const [result, setResult] = useState();
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+    
+          const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `Come up with a hex code for a ${adjectiveInput} shade of ${colorInput}`,
+            temperature: 0.6,
+          });
+          console.log(completion)
+    
+          setResult(completion.data.choices[0].text);
+        } catch(error) {
+          console.error(error);
+          alert(error.message);
+        }
+    }
+
+    return (
+        <div style={{width: "50%"}}>
+            <h3>Generate a</h3>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    name="adjective"
+                    placeholder="adjective"
+                    value={adjectiveInput}
+                    onChange={(e) => setAdjectiveInput(e.target.value)}
+                />
+                <h3>shade of</h3>
+                <input
+                    type="text"
+                    name="color"
+                    placeholder="color"
+                    value={colorInput}
+                    onChange={(e) => setColorInput(e.target.value)}
+                />
+                <input type="submit" value="Generate color" />
+            </form>
+            <div style={{backgroundColor: result}}>{result}</div>
+        </div>
+    );
+
+}
+
+export default ColorPrompter
---
+++ src/components/PalettePrompter/index.js
@@ -0,0 +1,60 @@
+import { React, useState } from 'react'
+import { Configuration, OpenAIApi } from "openai";
+
+
+const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY
+  });
+const openai = new OpenAIApi(configuration);
+  
+const PalettePrompter = () => {
+    const [numberInput, setNumberInput] = useState("");
+    const [adjectiveInput, setAdjectiveInput] = useState("");
+    const [result, setResult] = useState();
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+    
+          const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `Come up with hex codes for a ${adjectiveInput} palette of ${numberInput} colors`,
+            temperature: 0.6,
+          });
+          console.log(completion)
+    
+          setResult(completion.data.choices[0].text);
+        } catch(error) {
+          console.error(error);
+          alert(error.message);
+        }
+    }
+
+    return (
+        <div style={{width: "50%"}}>
+            <h3>Generate a</h3>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    name="adjective"
+                    placeholder="adjective"
+                    value={adjectiveInput}
+                    onChange={(e) => setAdjectiveInput(e.target.value)}
+                />
+                <h3>palette of</h3>
+                <input
+                    type="text"
+                    name="number"
+                    placeholder="number"
+                    value={numberInput}
+                    onChange={(e) => setNumberInput(e.target.value)}
+                />
+                <h3>colors</h3>
+                <input type="submit" value="Generate palete" />
+            </form>
+            <div style={{backgroundColor: result}}>{result}</div>
+        </div>
+    );
+
+}
+
+export default PalettePrompter
