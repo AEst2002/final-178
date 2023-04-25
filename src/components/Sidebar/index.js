648c7cf832dae533bbfaf14dcdd43f6b487600c4
Noa Kligfeld <noakligfeld@college.harvard.edu>
2023-04-25 14:38:59 -0400

    drag and drop example works

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,15 +4,91 @@ import ColorRow from '../ColorRow'
 import { ButtonPanel, Container, NameContainer } from './styles'
 import Edit from '../../assets/Edit.png'
 import Button from '../Button'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
   
 const Sidebar = ({favorites, setFavorites, currentColors, setCurrentColors}) => {
+    
+    const initialTasks = [
+        {
+            id: 1,
+            title: "test 1",
+        },
+        {
+            id: 2,
+            title: "test 2"
+        },
+        {
+            id: 3,
+            title: "test 3"
+        }
+    ]
+    const [tasks, setTasks] = useState(initialTasks)
+
+
+    const onDragEnd = (result) => {
+        if (!result.destination) return;
+        const items = Array.from(tasks);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+        setTasks(items)
+    }
+
     return (
         <Container>
             <NameContainer>
                 Palette #1
                 <CircleButton style={{marginLeft: '10px'}} icon={Edit} />
             </NameContainer>
+
+            <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="tasks">
+                {(provided) => (
+                    <div ref={provided.innerRef} {...provided.droppableProps}>
+                    {
+                        tasks.map((task, index) => (
+                            <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+                            {(provided) => (
+                                <div
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                ref={provided.innerRef}
+                                >
+                                <div>{task.title}</div>
+                                </div>
+                            )}
+                            </Draggable>
+                        ))
+                    }
+                    {provided.placeholder} 
+                    </div>
+                )}
+            </Droppable>
+                {/* <Droppable droppableId="tasks">
+                    {(provided) => (
+                        <div ref={provided.innerRef} {...provided.droppableProps}>
+                            {
+                                tasks.map((task, index) => (
+                                    <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+                                        {(provided => (
+                                            <div
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                                ref={provided.innerRef}
+                                            >
+                                                <div>{task.title}</div>
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                ));
+                            }
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable> */}
+
+            </DragDropContext>
+
             {currentColors.length ? currentColors.map((color, index) => {
                 return( 
                     <ColorRow 
