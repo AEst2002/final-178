439cf65c6881a6257c02febdbb828e3ccccc01fb
Noa Kligfeld <noakligfeld@college.harvard.edu>
2023-04-24 22:22:57 -0400

    added copy button and snackbar on copy

--- src/components/ColorRow/index.js
+++ src/components/ColorRow/index.js
@@ -10,9 +10,13 @@ import UpBlack from '../../assets/UpBlack.png'
 import DownBlack from '../../assets/DownBlack.png'
 import Trash from '../../assets/Trash.png'
 import textColor from '../../util/textColor'
+import CopyToClipboard from 'react-copy-to-clipboard'
+import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
+import { Snackbar } from '@mui/material'
 
 const ColorRow = ({favorites, setFavorites, hex, setCurrentColors, currentColors, index}) => {
     const [innerColor, setInnerColor] = useState('#000000')
+    const [copied, setCopied] = useState('false')
     useEffect(() => {
         setInnerColor(textColor(hex))
     }, [hex])
@@ -57,8 +61,19 @@ const ColorRow = ({favorites, setFavorites, hex, setCurrentColors, currentColors
             <CircleButton onClick={handleFavorite} style={{position: 'absolute', top: '10px', right: '10px'}} icon={favorites.includes(hex) ? HeartFilled : HeartEmpty}/>
             {index !== 0 && <Chevron onClick={() => handleMove('up')} style={{top: '45px', right: '10px'}} src={innerColor === '#000000' ? UpBlack : UpWhite}/>}
             {index !== (currentColors.length - 1) && <Chevron onClick={() => handleMove('down')} style={{top: '90px', right: '10px'}} src={innerColor === '#000000' ? DownBlack : DownWhite}/>}
-            <RowText fontSize={'30px'} color={innerColor}>{hex}</RowText>
+            <RowText fontSize={'30px'} color={innerColor} id={hex}>{hex}            
+                <CopyToClipboard style={{cursor: 'pointer', marginLeft: "7px"}} text={hex} onCopy={() => setCopied(true)}>
+                    <ContentCopyOutlinedIcon />
+                </CopyToClipboard>
+            </RowText>
             <RowText fontSize={'15px'} color={innerColor}>{ntc.name(hex)[1]}</RowText>
+            <Snackbar
+                message="Copied to clibboard"
+                anchorOrigin={{ vertical: "top", horizontal: "center" }}
+                autoHideDuration={2000}
+                onClose={() => setCopied(false)}
+                open={copied}
+            />
         </RowContainer>
     )
 
